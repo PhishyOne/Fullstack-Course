7c7da408a0cf88229d55ca0e39332ffbc4013c14
Chris Salmon <phishy0914@hotmail.com>
2025-08-12 02:02:51 +0000

    Continuing Lesson

--- 18 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ 18 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -1,6 +1,11 @@
 for (i = 0; i < document.querySelectorAll("button").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function() {
-        var audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
+    
+    //Set text color to white on click
+    this.style.color = "white";
+    
     });
 }
+
+// var audio = new Audio("./sounds/tom-1.mp3");
+// audio.play();
