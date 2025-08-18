9caa86bc5880f8f75b633bc8120df36951f4ba63
Chris Salmon <phishy0914@hotmail.com>
2025-08-18 06:30:31 +0000

    Project 20 Halfway

--- Completed-Projects/20-Simon/game.js
+++ Completed-Projects/20-Simon/game.js
@@ -1,1 +1,28 @@
-alert("hello");
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+function nextSequence() {
+    // Generate a random number between 1 and 4
+    var randomNumber = Math.floor(Math.random() * 3);
+    return randomNumber;
+}
+
+var randomChosenColor = buttonColors[nextSequence()];
+// Add the chosen color to the game pattern
+gamePattern.push(randomChosenColor);
+
+//Flash corresponding button
+$(".btn." + randomChosenColor).fadeOut(100).fadeIn(100);
+// Play sound for the chosen color
+var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+audio.play();
+
+//Detect button press
+$(".btn").click(function () { 
+    var userChosenColor = $(this).attr("id"); 
+    //Add chosen color to user clicked pattern
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+});
