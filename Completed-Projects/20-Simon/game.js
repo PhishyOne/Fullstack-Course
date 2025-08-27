3dd0c82227ce6c3b9e23a3f701560eebde635b10
Chris Salmon <phishy0914@hotmail.com>
2025-08-27 21:35:07 +0000

    Project 20 Getting to Meat and Potatoes

--- Completed-Projects/20-Simon/game.js
+++ Completed-Projects/20-Simon/game.js
@@ -2,27 +2,54 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
 
-function nextSequence() {
-    // Generate a random number between 1 and 4
-    var randomNumber = Math.floor(Math.random() * 3);
-    return randomNumber;
+function nextSequence() { 
+  // Generate a random number between 0 and 3
+  randomNumber = Math.floor((Math.random() * 3));
+  // Select a random color
+  randomChosenColor = buttonColors[randomNumber];
+  // Add each randomChosenColor to gamePattern
+  gamePattern.push(randomChosenColor);
+  // Select the button with the same id as the randomChosenColor
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  // Play the corresponding sound
+  playSound(randomChosenColor);
+  // Increase the level by 1
+  level++;
+  // Update the h1 with the new level
+  $("#level-title").text("Level " + level);
 }
 
-var randomChosenColor = buttonColors[nextSequence()];
-// Add the chosen color to the game pattern
-gamePattern.push(randomChosenColor);
+// Function for playing sounds
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
-//Flash corresponding button
-$(".btn." + randomChosenColor).fadeOut(100).fadeIn(100);
-// Play sound for the chosen color
-var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-audio.play();
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100); 
+}
 
-//Detect button press
-$(".btn").click(function () { 
-    var userChosenColor = $(this).attr("id"); 
-    //Add chosen color to user clicked pattern
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
+// Detect when a button is clicked
+$(".btn").click(function() {
+  // Store the id of the button that got clicked
+  var userChosenColor = $(this).attr("id");
+  // Add the contents of userChosenColor to a new array called userClickedPattern
+  userClickedPattern.push(userChosenColor);
+  // Play sound for the button that got clicked
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
 });
+
+// Detect if key has been pressed for the first time
+$(document).keypress(function() {
+  if (level === 0) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+  }
+});
+
