b7d86c861bee330aca9bd5764437842de9cb0439
Chris Salmon <phishy0914@hotmail.com>
2026-01-02 20:02:43 +0000

    Project 33-2 Start

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import project28Routes from "./routes/project28.js";
 import project29Routes from "./routes/project29.js";
 import project30Routes from "./routes/project30.js";
 import project331Routes from "./routes/project33-1.js";
+import project332Routes from "./routes/project33-2.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -25,6 +26,7 @@ app.use("/project29", project29Routes);
 app.use("/project30", project30Routes);
 app.use("/api", project30Routes);  
 app.use("/project33-1", project331Routes);
+app.use("/project33-2", project332Routes);
 
 // Root route
 app.get("/", (req, res) => {
@@ -33,7 +35,8 @@ app.get("/", (req, res) => {
     "<p><a href='/project28'>Project 28 - Secrets</a></p>" +
     "<p><a href='/project29'>Project 29 - Capstone Project - Eve Echoes PlayInt</a></p>" +
     "<p><a href='/project30'>Project 30 - Blog API</a></p>" +
-    "<p><a href='/project33-1'>Project 33-1 - Travel Tracker</a></p>");
+    "<p><a href='/project33-1'>Project 33-1 - Travel Tracker</a></p>" +
+    "<p><a href='/project33-1'>Project 33-2 - Family Travel Tracker</a></p>");
 });
 
 app.listen(port, () => {
---
+++ server/routes/project33-2.js
@@ -0,0 +1,76 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "ufp1ais6lj4me0",
+  host: "cet8r1hlj0mlnt.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
+  database: "daiavg0tvubkqb",
+  password: "p6c62e7eb19afc5f9566f7a7411bdd0454f02dd8c3094a7ec7d4056edb7f04edd",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+await db.connect();
+
+let currentUserId = 1;
+
+let users = [
+  { id: 1, name: "Angela", color: "teal" },
+  { id: 2, name: "Jack", color: "powderblue" },
+];
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: "teal",
+  });
+});
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/user", async (req, res) => {});
+
+app.post("/new", async (req, res) => {
+  //Hint: The RETURNING keyword can return the data that was inserted.
+  //https://www.postgresql.org/docs/current/dml-returning.html
+});
+
+export default router;
